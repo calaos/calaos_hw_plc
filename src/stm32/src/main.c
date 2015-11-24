4d96e54ad1ea8d46cf8af0766841af16d34777e3
Clement Leger <clement.leger@kalray.eu>
2015-11-24 18:39:33 +0100

    Better config support

--- src/stm32/src/main.c
+++ src/stm32/src/main.c
@@ -10,9 +10,8 @@ main()
 	hal_system_init();
 	
 	mysensor_init();
-	config_init();
 
-	mysensor_create_sensor(S_LIGHT, "SW1");
+	config_init();
 	
 	debug_puts("Entering main loop\r\n");
 	while(1) {
--- src/stm32/src/mysensor.c
+++ src/stm32/src/mysensor.c
@@ -1,5 +1,6 @@
 #include "HAL.h"
 #include "debug.h"
+#include "utils.h"
 #include "module.h"
 #include "mysensor.h"
 
@@ -7,9 +8,53 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 static unsigned int current_sensor_id = 0;
 static unsigned int assigned_node_id;
+
+static const char *sensor_type_str[] = {
+    [0] = "S_DOOR",                 // Door and window sensors
+    [1] = "S_MOTION",               // Motion sensors
+    [2] = "S_SMOKE",                // Smoke sensor
+    [3] = "S_LIGHT",                // Light Actuator (on/off)
+    [4] = "S_DIMMER",               // Dimmable device of some kind
+    [5] = "S_COVER",                // Window covers or shades
+    [6] = "S_TEMP",                 // Temperature sensor
+    [7] = "S_HUM",                  // Humidity sensor
+    [8] = "S_BARO",                 // Barometer sensor (Pressure)
+    [9] = "S_WIND",                 // Wind sensor
+    [10] = "S_RAIN",                // Rain sensor
+    [11] = "S_UV",                  // UV sensor
+    [12] = "S_WEIGHT",              // Weight sensor for scales etc.
+    [13] = "S_POWER",               // Power measuring device, like power meters
+    [14] = "S_HEATER",              // Heater device
+    [15] = "S_DISTANCE",            // Distance sensor
+    [16] = "S_LIGHT_LEVEL",         // Light sensor
+    [17] = "S_ARDUINO_NODE",        // Arduino node device
+    [18] = "S_ARDUINO_RELAY",       // Arduino relaying node device
+    [19] = "S_LOCK",                // Lock device
+    [20] = "S_IR",                  // Ir sender/receiver device
+    [21] = "S_WATER",               // Water meter
+    [22] = "S_AIR_QUALITY",         // Air quality sensor e.g. MQ-2
+    [23] = "S_CUSTOM",              // Use this for custom sensors where no other fits.
+    [24] = "S_DUST",                // Dust level sensor
+    [25] = "S_SCENE_CONTROLLER",    // Scene controller device
+};
+
+
+static int mysensor_typestr_to_type(const char *name)
+{
+	unsigned int i;
+
+	for (i = 0; i < ARRAY_SIZE(sensor_type_str); i++) {
+		if (strcmp(sensor_type_str[i], name) == 0) {
+			return i;
+		}
+	}
+
+	return -1;
+}
 
 static int
 mysensor_send_raw_message(uint16_t node_id,
@@ -75,16 +120,36 @@ mysensor_update_value_str(mysensor_sensor_t *s, mysensor_datatype_t dt, char *st
 	return mysensor_send_message(s->node_id, SET_VARIABLE, REQUEST, dt, str);
 }
 
+
 static int
 mysensor_config_handler(const char* section, const char* name, const char* value)
 {
+	static bool s_fill = 0;
+	static int s_type;
+	static char s_name[MYSENSOR_MAX_NAME_LENGTH];
+
+	/* We only handle sensor sections */
 	if (strcmp(section, "sensor") != 0)
 		return 0;
 
-	if (name != NULL)
-		debug_puts("Name %s, value %s\r\n", name, value);
-	else
-		debug_puts("Section: %s\r\n", section);
+	/* New sensor section */
+	if (name == NULL) {
+		/* If we are filling a sensor */
+		if (s_fill) {
+			mysensor_create_sensor(s_type, s_name);
+			s_fill = false;
+		} else {
+			s_fill = true;
+		}
+	} else {
+		if (strcmp(name, "type") == 0) {
+			mysensor_typestr_to_type(value);
+		} else if (strcmp(name, "name") == 0) {
+			strncpy(s_name, value, MYSENSOR_MAX_NAME_LENGTH);
+		} else {
+			debug_puts("Unknown option %s\r\n", name);
+		}
+	}
 
 	return 1;
 }
