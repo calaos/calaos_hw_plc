1f94c31143f26880d2dd567620980aa0306ba44a
Clement Leger <clement.leger@kalray.eu>
2016-01-03 11:27:39 +0100

    Config: add support for external config

--- src/stm32/src/core/config.c
+++ src/stm32/src/core/config.c
@@ -7,6 +7,7 @@
 #include "debug.h"
 #include "config.h"
 #include "module.h"
+#include "utils.h"
 
 #define DEFAULT_BAUDRATE	115200
 
@@ -23,22 +24,37 @@ config_init()
 	json_char *buffer = NULL;
 	int length = 0;
 	char filename[FILENAME_MAX_LENGTH];
+
 	debug_puts("Config init\r\n");
 
 	snprintf(filename, FILENAME_MAX_LENGTH, "%s"CONFIG_NAME, hal_get_filesystem_prefix());
 
 	config = fopen(filename, "r");
 	if (config == NULL) {
+		debug_puts("Using default configuration\r\n");
 		buffer = (json_char *) &__config_start;
 		length = &__config_end - &__config_start;
-		debug_puts("Using default configuration");
+	} else {
+		fseek(config, 0, SEEK_END);
+
+		length = ftell(config);
+		rewind(config);
+		buffer = malloc(length);
+		if (!buffer)
+			PANIC("Can not allocate enough memory\r\n");
+
+		fread(buffer, 1, length, config);
 	}
 
 	value = json_parse(buffer, length);
 
-        if (value == NULL) {
-                debug_puts("Unable to parse data\r\n");
-        }
+        if (value == NULL)
+                PANIC("Unable to parse data\r\n");
+
+	if (config != NULL) {
+		free(buffer);
+		fclose(config);
+	}
 
 	module_json_parse(value);
 
