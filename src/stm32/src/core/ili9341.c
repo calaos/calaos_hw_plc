8f10cb9b7292e777fdb102e41c0594b758a72257
Clément Léger <clement.leger@kalray.eu>
2016-01-21 00:13:41 +0100

    ILI9341: take stuff from adafruit ili9314 driver

--- src/stm32/src/core/ili9341.c
+++ src/stm32/src/core/ili9341.c
@@ -10,123 +10,88 @@ static uint32_t g_ili9341_width, g_ili9341_height;
 static gen_io_t *g_ili9341_cs = NULL, *g_ili9341_dc = NULL, *g_ili9341_rst = NULL;
 
 static void
-ili9341_send(int dc, uint8_t value)
-{   
-    /* register or data */
-    gen_io_write(g_ili9341_dc, dc);
-
-    /* transmit via SPI */
-    gen_io_write(g_ili9341_cs, 0);
-    hal_spi_write(value);
-    gen_io_write(g_ili9341_cs, 1);
+ili9341_send_reg(uint8_t reg)
+{
+	/* write register */
+	gen_io_write(g_ili9341_dc, ILI9341_REG);
+	gen_io_write(g_ili9341_cs, 0);
+	hal_spi_write(reg);
+	gen_io_write(g_ili9341_cs, 1);
 }
 
-struct ili9341_command {
-	char type;
-	char value;
-};
-
-static const struct ili9341_command ili9341_init_cmds[] =
+static void
+ili9341_send_data(const uint8_t *value, uint8_t size)
 {
-	{ILI9341_REG,  0xCB},
-	{ILI9341_DATA, 0x39},
-	{ILI9341_DATA, 0x2C},
-	{ILI9341_DATA, 0x00},
-	{ILI9341_DATA, 0x34},
-	{ILI9341_DATA, 0x02},
+	unsigned int i;
+	/* transmit via SPI */
+	gen_io_write(g_ili9341_cs, 0);
+	
+	gen_io_write(g_ili9341_dc, ILI9341_DATA);    
+	for (i = 0; i < size; i++)
+		hal_spi_write(value[i]);
 
-	{ILI9341_REG,  0xCF},
-	{ILI9341_DATA, 0x00},
-	{ILI9341_DATA, 0XC1},
-	{ILI9341_DATA, 0X30},
+	gen_io_write(g_ili9341_cs, 1);
+}
 
-	{ILI9341_REG,  0xE8},
-	{ILI9341_DATA, 0x85},
-	{ILI9341_DATA, 0x00},
-	{ILI9341_DATA, 0x78},
+static void
+ili9341_send_reg_data(uint8_t reg, const uint8_t *value, uint8_t size)
+{
+	unsigned int i;
+	/* transmit via SPI */
+	gen_io_write(g_ili9341_cs, 0);
 
-	{ILI9341_REG,  0xEA},
-	{ILI9341_DATA, 0x00},
-	{ILI9341_DATA, 0x00},
+	gen_io_write(g_ili9341_dc, ILI9341_REG);
+	ili9341_send_reg(reg);
+	
+	gen_io_write(g_ili9341_dc, ILI9341_DATA);    
+	for (i = 0; i < size; i++)
+		hal_spi_write(value[i]);
 
-	{ILI9341_REG,  0xED},
-	{ILI9341_DATA, 0x64},
-	{ILI9341_DATA, 0x03},
-	{ILI9341_DATA, 0X12},
-	{ILI9341_DATA, 0X81},
+	gen_io_write(g_ili9341_cs, 1);
+}
 
-	{ILI9341_REG,  0xF7},
-	{ILI9341_DATA, 0x20},
 
-	{ILI9341_REG,  0xC0}, // Power control
-	{ILI9341_DATA, 0x23}, // VRH[5:0]
+static const uint8_t ili9341_init_cmds[] =
+{
+	3, 0xEF,  0x03, 0x80, 0x02,
+	// Power Control B
+	3, 0xCF, 0x00, 0XC1, 0X30,
+	// Power On sequence control
+	4, 0xED, 0x64, 0x03, 0X12, 0X81,
+	// Driver timing control A
+	3, 0xE8, 0x85, 0x00, 0x78,
 
-	{ILI9341_REG,  0xC1}, // Power control
-	{ILI9341_DATA, 0x10}, // SAP[2:0];BT[3:0]
+	5, 0xCB, 0x39, 0x2C, 0x00, 0x34, 0x02,
+ 
+	1, 0xF7, 0x20,
 
-	{ILI9341_REG,  0xC5}, // VCM control
-	{ILI9341_DATA, 0x3e},
-	{ILI9341_DATA, 0x28},
+	2, 0xEA, 0x00, 0x00,
+	//VRH[5:0] 
+	1, ILI9341_PWCTR1, 0x23, 
+	//SAP[2:0];BT[3:0] 
+	1, ILI9341_PWCTR2, 0x10,  
+	//VCM control
+	2, ILI9341_VMCTR1, 0x3e, 0x28,
+	//VCM control2 
+	1, ILI9341_VMCTR2, 0x86,
+	// Memory Access Control 
+	1, ILI9341_MADCTL, 0x48,
 
-	{ILI9341_REG,  0xC7}, // VCM control2
-	{ILI9341_DATA, 0x86},
+	1, ILI9341_PIXFMT, 0x55,
+  
+	2, ILI9341_FRMCTR1, 0x00, 0x18,
+	// Display Function Control
+	3, ILI9341_DFUNCTR, 0x08, 0x82, 0x27,    
+	// 3Gamma Function Disable
+	1, 0xF2, 0x00,   
+	//Gamma curve selected
+	1, ILI9341_GAMMASET, 0x01,
+	//Set Gamma 
+	15, ILI9341_GMCTRP1, 0x0F, 0x31, 0x2B, 0x0C, 0x0E, 0x08, 0x4E, 0xF1, 0x37, 0x07, 0x10, 0x03, 0x0E, 0x09, 0x00,
+	//Set Gamma
+	15, ILI9341_GMCTRN1, 0x00, 0x0E, 0x14, 0x03, 0x11, 0x07, 0x31, 0xC1, 0x48, 0x08, 0x0F, 0x0C, 0x31, 0x36, 0x0F,
 
-	{ILI9341_REG,  0x36}, // Memory Access Control
-	{ILI9341_DATA, 0x88}, // C8
-
-	{ILI9341_REG,  0x3A},
-	{ILI9341_DATA, 0x55},
-
-	{ILI9341_REG,  0xB1},
-	{ILI9341_DATA, 0x00},
-	{ILI9341_DATA, 0x18},
-
-	{ILI9341_REG,  0xB6}, // Display Function Control
-	{ILI9341_DATA, 0x08},
-	{ILI9341_DATA, 0x82},
-	{ILI9341_DATA, 0x27},
-
-	{ILI9341_REG,  0xF2}, // 3Gamma Function Disable
-	{ILI9341_DATA, 0x00},
-
-	{ILI9341_REG,  0x26}, // Gamma curve selected
-	{ILI9341_DATA, 0x01},
-
-	{ILI9341_REG,  0xE0}, // Set Gamma
-	{ILI9341_DATA, 0x0F},
-	{ILI9341_DATA, 0x31},
-	{ILI9341_DATA, 0x2B},
-	{ILI9341_DATA, 0x0C},
-	{ILI9341_DATA, 0x0E},
-	{ILI9341_DATA, 0x08},
-	{ILI9341_DATA, 0x4E},
-	{ILI9341_DATA, 0xF1},
-	{ILI9341_DATA, 0x37},
-	{ILI9341_DATA, 0x07},
-	{ILI9341_DATA, 0x10},
-	{ILI9341_DATA, 0x03},
-	{ILI9341_DATA, 0x0E},
-	{ILI9341_DATA, 0x09},
-	{ILI9341_DATA, 0x00},
-
-	{ILI9341_REG,  0xE1}, // Set Gamma
-	{ILI9341_DATA, 0x00},
-	{ILI9341_DATA, 0x0E},
-	{ILI9341_DATA, 0x14},
-	{ILI9341_DATA, 0x03},
-	{ILI9341_DATA, 0x11},
-	{ILI9341_DATA, 0x07},
-	{ILI9341_DATA, 0x31},
-	{ILI9341_DATA, 0xC1},
-	{ILI9341_DATA, 0x48},
-	{ILI9341_DATA, 0x08},
-	{ILI9341_DATA, 0x0F},
-	{ILI9341_DATA, 0x0C},
-	{ILI9341_DATA, 0x31},
-	{ILI9341_DATA, 0x36},
-	{ILI9341_DATA, 0x0F},
-
-	{ILI9341_REG,  0x11}, // Sleep out
+	0, ILI9341_SLPOUT,   //Exit Sleep 
 };
 	
 static void
@@ -134,10 +99,9 @@ ili9341_init(int width, int height)
 {
 	g_ili9341_width = width;
 	g_ili9341_height = height;
-	unsigned int i;
+	unsigned int i = 0;
 
 	if (g_ili9341_rst != NULL) {
-		gen_io_write(g_ili9341_rst, 1);
 		ms_delay(5);
 		gen_io_write(g_ili9341_rst, 0);
 		ms_delay(20);
@@ -145,68 +109,46 @@ ili9341_init(int width, int height)
 		ms_delay(150);
 	}
 
-	for (i = 0; i < ARRAY_SIZE(ili9341_init_cmds); i++) {
-		ili9341_send(ili9341_init_cmds[i].type, ili9341_init_cmds[i].value); 
+	while (i < ARRAY_SIZE(ili9341_init_cmds)) {
+		ili9341_send_reg_data(ili9341_init_cmds[i + 1], &ili9341_init_cmds[i + 2], ili9341_init_cmds[i]);
+		i += ili9341_init_cmds[i] + 1;
 	}
 
 	ms_delay(120);
 
-	ili9341_send(ILI9341_REG, 0x2c);
-
-	//lcd_fill(global_bg_color);
-    
-	ili9341_send(ILI9341_REG, 0x29); // Display on 
-	ili9341_send(ILI9341_REG, 0x2c);
+	ili9341_send_reg(ILI9341_DISPON);   //Display on 
+	ili9341_send_reg(ILI9341_RAMWR);
 }
 
 static void
-ili9341_set_cursor_x(uint16_t x)
+ili9341_set_addr_window(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1)
 {
-	if (x >= g_ili9341_width)
-		return;
-    
-	ili9341_send(ILI9341_REG, 0x2B);
-	ili9341_send(ILI9341_DATA, x >> 8);
-	ili9341_send(ILI9341_DATA, x & 0xFF);
-	ili9341_send(ILI9341_REG, 0x2c);
-}
+	uint8_t x_cmd[4] = {x0 >> 8, x0 & 0xFF, x1 >> 8, x1 & 0xFF};
+	uint8_t y_cmd[4] = {y0 >> 8, y0 & 0xFF, y1 >> 8, y1 & 0xFF};
 
-static void
-ili9341_set_cursor_y(uint16_t y)
-{
-	if (y >= g_ili9341_height)
-		return;
-
-	ili9341_send(ILI9341_REG, 0x2A);
-	ili9341_send(ILI9341_DATA, y >> 8);
-	ili9341_send(ILI9341_DATA, y & 0xFF);
-	ili9341_send(ILI9341_REG, 0x2c);
-}
-
-static void
-ili9341_set_cursor(uint16_t x, uint16_t y)
-{
-	ili9341_set_cursor_x(x);
-	ili9341_set_cursor_y(y);
-}
-
-static void
-ili9341_send_pixel(uint16_t color)
-{
-    ili9341_send(ILI9341_DATA, color >> 8);
-    ili9341_send(ILI9341_DATA, color & 0xFF);
+	ili9341_send_reg_data(ILI9341_CASET, x_cmd, 4);
+	ili9341_send_reg_data(ILI9341_PASET, y_cmd, 4);
+	ili9341_send_reg(ILI9341_RAMWR);
 }
 
 static void
 ili9341_draw_pixel(int x, int y, uint16_t color)
 {
-	ili9341_set_cursor(x, y);
-	ili9341_send_pixel(color);
+	uint8_t data[2] = {color >> 8, color & 0xFF};
+
+	ili9341_set_addr_window(x, y, x+1, y+1);
+	ili9341_send_data(data, 2);
+}
+
+// Pass 8-bit (each) R,G,B, get back 16-bit packed color
+uint16_t ili9341_color_from_rgb(uint8_t r, uint8_t g, uint8_t b) {
+	return ((r & 0xF8) << 8) | ((g & 0xFC) << 3) | (b >> 3);
 }
 
 static void
 ili9341_display()
 {
+	/* nothing to do, no framebuffer */
 }
 
 static void
@@ -234,6 +176,9 @@ ili9341_parse_json(json_value *disp_data)
 
 	/* Deselect cs */
 	gen_io_write(g_ili9341_cs, 1);
+	/* Deassert reset */
+	if (g_ili9341_rst != NULL)
+		gen_io_write(g_ili9341_rst, 1);
 }
 
 display_ops_t ili9341_display_ops = {
